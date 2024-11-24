e9390ad6b4afb5d28613bc0be9690c5bdb441ef3
Xianliang Yang <xianya@microsoft.com>
2024-11-25 00:34:51 +0800

    average score

--- data_collection.py
+++ data_collection.py
@@ -15,7 +15,7 @@ def parse_arguments():
     parser.add_argument("-p", "--problem", choices=problem_pool, required=True, help="Type of problem to solve.")
     parser.add_argument("-d", "--data_path", type=str, required=True, help="Path for source data.")
     parser.add_argument("-e", "--heuristic_dir", default=None, help="Path of heuristic dir.")
-    parser.add_argument("-s", "--search_time", default=1000, help="MCTS times for each heuristic.")    
+    parser.add_argument("-s", "--search_time", default=1000, type=int, help="MCTS times for each heuristic.")    
     parser.add_argument("-sc", "--score_calculation", choices=["average_score", "a8t2"], default="average_score", help="Function to calculate score.")
     parser.add_argument("-pf", "--prune_frequency", default=200, help="Prune and early stop frequency.")
     parser.add_argument("-pr", "--prune_ratio", default=1.02, help="Prune and early stop threshold.")
@@ -124,22 +124,27 @@ def data_collection(
         saved_solution = copy.deepcopy(env.current_solution)
         saved_state = copy.deepcopy(env.state_data)
         saved_algorithm_data = copy.deepcopy(env.algorithm_data)
-        is_complete = env.is_complete_solution
-        all_operators = []
-
-        output_file = open(os.path.join(output_dir, f"round_{round_index}.txt"), "w")
-        output_file.write(f"selected_previous_heuristics\toperators: \n{selected_previous_heuristics_str}\n")
-        output_file.write(f"current_solution: \n{saved_solution}\n")
+        saved_key_value = env.key_value
+        saved_is_complete = env.is_complete_solution
 
         best_score = np.inf
+        best_results = []
+        output_file = open(os.path.join(output_dir, f"round_{round_index}.txt"), "w")
+        output_file.write(f"selected_previous_heuristics\toperators\n{selected_previous_heuristics_str}")
         output_file.write("---------------\n")
-        output_file.write(f"heuristic\t{score_calculation.__name__}\tresults\n")
+        output_file.write(f"current_solution: \n{saved_solution}\n")
+        output_file.write(f"is_complete_solution\t{saved_is_complete}\n")
+        if saved_is_complete:
+            output_file.write(f"key_value\t{saved_key_value}\n")
+
+        output_file.write("---------------\n")
+        output_file.write(f"heuristic\t{score_calculation.__name__}\tany_operator\tresults\n")
         for heuristic_file in os.listdir(heuristic_dir):
             env.reset()
             heuristic_name = heuristic_file.split(".")[0]
-
             results = []
             operators = []
+            
             for search_index in range(search_time):
                 env.current_solution = saved_solution
                 env.state_data = saved_state
@@ -149,27 +154,32 @@ def data_collection(
                 random_hh.run(env)
                 results.append(env.key_value)
                 if (search_index + 1) % prune_ratio == 0:
-                    env.compare(best_score * prune_ratio, score_calculation(results)) > 0
-                    break
+                    if env.compare(best_score * prune_ratio, score_calculation(results)) > 0:
+                        break
             score = score_calculation(results)
             results_str = ",".join([str(result) for result in sorted(results)])
-            output_file.write(f"{heuristic_name}\t{score}\t{results_str}\n")
-            all_operators.extend(operators)
+            any_operator = any(operators)
+            output_file.write(f"{heuristic_name}\t{score}\t{any_operator}\t{results_str}\n")
 
-            if env.compare(score, best_score) > 0:
+            if env.compare(score, best_score) > 0 and any_operator:
                 best_score = score
                 best_heuristics = heuristic_name
-                next_operator = operator
+                next_operator = next((operator for operator in operators if operator is not False), None)
                 best_operators_str = [str(operator) for operator in operators]
+                best_results = results
         selected_previous_heuristics.append(best_heuristics)
         previous_operator.append(next_operator)
         output_file.write("---------------\n")
-        output_file.write(f"best_heuristics:\t{best_heuristics}\n")
-        output_file.write(f"best_operators:\t{best_operators_str}\n")
-        output_file.write(f"next_operator:\t{next_operator}\n")
+        if saved_is_complete and env.is_complete_solution:
+            if len(best_results) == 0 or (env.compare(saved_key_value, min(best_results)) >= 0 and env.compare(saved_key_value, max(best_results)) >= 0):
+                output_file.write(f"Stop\n")
+                output_file.close()
+                break
+        output_file.write(f"best_heuristics\t{best_heuristics}\n")
+        output_file.write(f"best_operators\t{best_operators_str}\n")
+        output_file.write(f"next_operator\t{next_operator}\n")
         output_file.close()
-        if is_complete and not any(all_operators):
-            break
+
     output_file = open(os.path.join(output_dir, f"finished.txt"), "w")
     output_file.write(f"Total rounds: {round_index}")
     output_file.close()
