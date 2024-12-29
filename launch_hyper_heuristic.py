883ca4a8056b64cbe04bb12bb372631bff790a40
VictorYXL <1601210807@pku.edu.cn>
2024-12-29 10:21:24 +0800

    fix launcher

--- launch_hyper_heuristic.py
+++ launch_hyper_heuristic.py
@@ -32,7 +32,7 @@ def main():
         test_dir = os.path.join("output", problem, "data", "test_data") if args.test_dir is None else args.test_dir
     test_cases = os.listdir(test_dir) if test_case is None else [test_case]
     datetime_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_dir = args.output_dir if args.output_dir is not None else f"{heuristic}.{datetime_str}"
+    output_dir = args.output_dir if args.output_dir is not None else f"{heuristic}"
 
     if heuristic_dir is None:
         heuristic_dir = os.path.join("src", "problems", problem, "heuristics", "basic_heuristics")
