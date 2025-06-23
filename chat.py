daf1474b0694e11e878daee32b764a55ac2578ea
Xianliang Yang <xianya@microsoft.com>
2025-06-23 20:40:30 +0800

    update

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 import os
 from src.util.llm_client.get_llm_client import get_llm_client
 
-config_file = os.path.join("output", "llm_config", "azure_gpt_4o.json")
+config_file = os.path.join("output", "llm_config", "azure_gpt_o3.json")
 llm_client = get_llm_client(config_file=config_file)
 llm_client.load("Hi. Are you awake?")
 response = llm_client.chat()
